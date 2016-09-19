dcc93ee1b73f5cda32de0df7077faac4c8179c2a
IvanOleynik <ivanbox2401@gmail.com>
2016-09-19 14:18:55 +0300

    Урок 2. Задание 1.

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by www on 19.09.2016.
+ */
+public class PointTests {
+
+  @Test
+  public void testDistance() {
+    Point p1 = new Point(2,3);
+    //Point p2 = new Point(8,32);
+    Assert.assertEquals(p1.distance(), 2.23606797749979);
+
+
+  }
+
+}
