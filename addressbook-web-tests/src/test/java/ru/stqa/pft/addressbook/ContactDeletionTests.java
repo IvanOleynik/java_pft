9cbce2d63f263757c3bd09e530f003a7290c5be5
IvanOleynik <ivanbox2401@gmail.com>
2016-09-27 09:30:58 +0300

    Добавление теста (модификация группы) без рекордера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
@@ -22,15 +22,8 @@ public class ContactDeletionTests {
     }
     
     @Test
-    public void ContactDeletionTests() {
-        wd.get("http://localhost/addressbook/group.php");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    public void testContactDeletion() {
+
         wd.findElement(By.id("container")).click();
         wd.findElement(By.linkText("home")).click();
         if (!wd.findElement(By.id("2")).isSelected()) {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by www on 27.09.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+
+  public void testGroupModficatiion() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification ();
+    app.getGroupHelper().returnToGroupPage();
+
+  }
+
+}
