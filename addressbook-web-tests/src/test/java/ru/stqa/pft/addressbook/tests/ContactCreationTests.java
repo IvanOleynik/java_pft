33df7ab32464d20f653c34665894940d84500b60
IvanOleynik <ivanbox2401@gmail.com>
2016-10-03 10:57:52 +0300

    Добавлен подсчет элементов на страницах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,10 +9,13 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-
+    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().gotoAddNewContact();
     app.getContactHelper().createContact(new ContactData("Иван", "Семенович", "Голопуцков", "golopuc",
             "Свободная Касса", "Петровка, 38", "8(999)3883883", "golopuc@gmail.com", "test1"));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before +1);
+
 
   }
 
