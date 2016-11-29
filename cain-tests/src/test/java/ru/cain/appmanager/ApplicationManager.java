7a77db53c27c1eb09ad87bca586f52b99ad1ff97
IvanOleynik <ivanbox2401@gmail.com>
2016-11-29 18:17:44 +0300

    обновление сборщика

--- cain-tests/src/test/java/ru/cain/appmanager/ApplicationManager.java
+++ cain-tests/src/test/java/ru/cain/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.cain.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +8,6 @@ import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
